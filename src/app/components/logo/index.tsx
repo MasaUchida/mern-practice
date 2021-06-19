fb88b98a6698c9a94749eea3c4b514d274b91dca
Masatoshi Uchida <uchidamasa.design@gmail.com>
2021-06-20 00:53:19 +0900

    finish car section

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -37,7 +37,7 @@ const Logo = () => {
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImg} />
+        <img src={CarLogoImg} alt="" />
       </Image>
       <LogoText>yourcar.</LogoText>
     </LogoContainer>
